ceb3f8e9e11f206e0b0296492a318275d5dcedd6
Palash Nath <palashnath880@gmail.com>
2023-12-04 22:54:29 +0600

    Hero and About Area Responsive Layout Updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,15 +4,15 @@ import React from 'react';
 const About = () => {
     return (
         <section>
-            <div className='container mx-auto px-5 py-20'>
-                <div className='flex items-center gap-20'>
-                    <div className='w-1/2'>
+            <div className='container mx-auto px-5 max-md:pb-0 py-20'>
+                <div className='flex items-center max-md:flex-col-reverse max-md:gap-10 gap-20'>
+                    <div className='max-md:w-full w-1/2'>
                         <div className='flex flex-col gap-5 items-start'>
-                            <h4 className='text-4xl font-semibold text-gold leading-snug'>Welcome to Palash&apos;s <span className='text-secondary'>Web Development</span> World!</h4>
+                            <h4 className='text-4xl max-sm:text-3xl font-semibold text-gold leading-snug'>Welcome to Palash&apos;s <span className='text-secondary'>Web Development</span> World!</h4>
                             <p className='text-base text-gold'>Hello, I&apos;m Palash Nath, a passionate web developer with a knack for turning ideas into digital realities. From pixel-perfect design to seamless functionality, I specialize in creating immersive and dynamic web experiences.</p>
                         </div>
                     </div>
-                    <div className='w-1/2'>
+                    <div className='max-md:w-full w-1/2'>
                         <div className='w-full aspect-video overflow-hidden rounded-xl'>
                             <Image src='/images/programmer-image.jpg' alt='Palash Nath' width={400} height={400} className='w-full h-full object-cover' />
                         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,11 @@ const Hero = () => {
         <section className='h-[95vh]'>
             <div className='w-full h-full pt-20' style={{ backgroundImage: `linear-gradient(#00000085,#00000085), url(/images/next-coding.webp)` }}>
                 <div className='container mx-auto px-5 w-full h-full'>
-                    <div className='w-full h-full flex items-center gap-10'>
-                        <div className='w-1/2'>
-                            <div className='flex flex-col gap-8 items-start'>
-                                <p className='text-white text-xl'>Crafting Digital Experiences, One Line of Code at a Time.</p>
-                                <h2 className='text-5xl text-white font-semibold leading-tight'>Hi, I&apos;m Palash, a Passionate <span className='text-gold opacity-90'>Web Developer.</span></h2>
+                    <div className='w-full h-full flex max-md:flex-col items-center gap-10'>
+                        <div className='max-md:w-full w-1/2'>
+                            <div className='flex flex-col gap-8 items-start max-sm:items-center'>
+                                <p className='text-white max-sm:text-base text-xl max-md:text-center '>Crafting Digital Experiences, One Line of Code at a Time.</p>
+                                <h2 className='text-5xl max-sm:text-3xl text-white max-md:text-center font-semibold leading-snug'>Hi, I&apos;m Palash, a Passionate <span className='text-gold opacity-90'>Web Developer.</span></h2>
                                 <button className='text-gold font-medium border border-gold px-7 py-3 hover:bg-gold duration-200 hover:text-white'>Explore My Work</button>
                             </div>
                         </div>
